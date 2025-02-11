9bd159149d02149040c7a824b79fe9fe3fdd58b2
mpdev-id <mnprasetya@gmail.com>
2025-02-11 16:12:30 +0700

    2024.02.11 - let`s play with oneSignal

--- app/Models/User.php
+++ app/Models/User.php
@@ -44,4 +44,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function routeNotificationForOneSignal()
+    {
+        return $this->onesignal_player_id;
+    }
 }
--- app/Notifications/EventReminder.php
+++ app/Notifications/EventReminder.php
@@ -4,15 +4,16 @@ namespace App\Notifications;
 
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
+use Illuminate\Notifications\Messages\MailMessage;
+use Berkayk\OneSignal\OneSignalChannel;
+use Berkayk\OneSignal\OneSignalMessage;
 
-class EventReminder extends Notification
+class EventReminder extends Notification implements ShouldQueue
 {
     use Queueable;
 
     protected $event;
-
 
     public function __construct($event)
     {
@@ -21,9 +22,10 @@ class EventReminder extends Notification
 
     public function via($notifiable)
     {
-        return ['mail'];
+        return ['mail', OneSignalChannel::class];
     }
 
+    // Method untuk email
     public function toMail($notifiable)
     {
         return (new MailMessage)
@@ -33,5 +35,17 @@ class EventReminder extends Notification
             ->line("Don't forget to prepare for it!")
             ->action('View Event', url('/events/' . $this->event->id))
             ->line('Thank you for using our application!');
+    }
+
+    // Method untuk OneSignal
+    public function toOneSignal($notifiable)
+    {
+        
+        return OneSignalMessage::create()
+            ->setSubject("Reminder: {$this->event->name} is Coming Soon!")
+            ->setBody("Your event \"{$this->event->title}\" will start on {$this->event->start}. Don't forget to prepare for it!")
+            ->setUrl(url('/events/' . $this->event->id))
+            ->setData('event_id', $this->event->id)
+            ->setData('type', 'event_reminder');
     }
 }
